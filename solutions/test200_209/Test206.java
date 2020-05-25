aca0834dc26c6ad35099899f39aab5c38266e7fa
WOQUQ <zhouzhiquq@gmail.com>
2020-05-25 11:43:11 +0800

    add the test 206

--- solutions/test200_209/Test206.java
+++ solutions/test200_209/Test206.java
@@ -1,7 +1,41 @@
 package test200_209;
 
 public class Test206 {
+    //迭代
     public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
 
+        ListNode pre = head;
+        ListNode curr = head.next;
+        if(curr == null) return head;
+        head.next = null;
+        while(curr.next != null){
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        curr.next = pre;
+
+        return curr;
+    }
+
+    //递归
+    public ListNode reverseList2(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode curr = head.next;
+        head.next = null;
+        return helper(curr,head);
+        
+    }
+
+    private ListNode helper(ListNode curr,ListNode pre){
+        if(curr.next == null){
+            curr.next = pre;
+            return curr;
+        }
+        ListNode result= helper(curr.next,curr);
+        curr.next = pre;
+        return result;
     }
 }
