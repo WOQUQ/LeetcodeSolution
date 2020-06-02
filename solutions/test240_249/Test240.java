202f8525859fabf11dc811edc1965f0127cc1385
WOQUQ <zhouzhiquq@gmail.com>
2020-06-02 14:04:00 +0800

    test 240

---
+++ solutions/test240_249/Test240.java
@@ -0,0 +1,21 @@
+package test240_249;
+
+public class Test240 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if( matrix.length == 0 || matrix[0].length == 0) return false;
+
+        int i = matrix.length - 1;
+        int j = 0;
+
+        while(i >= 0 && j < matrix[0].length){
+            if(matrix[i][j] > target)
+                i--;
+            else if(matrix[i][j] < target)
+                j++;
+            else
+                return true;
+        }
+
+        return false;
+    }
+}
