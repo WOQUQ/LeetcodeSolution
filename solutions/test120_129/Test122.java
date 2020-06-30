606451e891cbbc91f6159b35e25e38e6fe4e2b9e
WOQUQ <zhouzhiquq@gmail.com>
2020-06-30 20:58:03 +0800

    test 122

--- solutions/test120_129/Test122.java
+++ solutions/test120_129/Test122.java
@@ -1,7 +1,12 @@
 package test120_129;
 
 public class Test122 {
-    // public int maxProfit(int[] prices) {
-
-    // }
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
 }
