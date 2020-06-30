90f904fdb9f3b8f285d0abfd18151e0f3bd82e54
WOQUQ <zhouzhiquq@gmail.com>
2020-06-30 20:10:31 +0800

    test 235

--- solutions/test230_239/Terst235.java
+++ solutions/test230_239/Terst235.java
@@ -2,6 +2,14 @@ package test230_239;
 
 public class Terst235 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+        while(true){
+            if(root.val > p.val && root.val > q.val){
+                root = root.left;
+            }else if(root.val < p.val && root.val < q.val){
+                root = root.right;
+            }else{
+                return root;
+            }
+        }
     }
 }
